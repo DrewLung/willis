e638508e9014c692de658409ca9d8dbf0e4bcb48
Cloud User <ec2-user@ip-172-31-33-138.us-west-2.compute.internal>
2016-10-13 18:44:27 -0400

    tax homework finished

--- tax_script.sh
+++ tax_script.sh
@@ -1,15 +1,20 @@
 function nextBracket {
-  echo "nextBracket"
+  income=$1
+  topOfBracket=$2
+  taxRate=$3
+  echo "your marginal tax rate is : $taxRate"
+  (( delta = $topOfBracket - $income ))
+  echo "$delta more dollars until the next bracket"
 }
 
 read -p "What is your Federal Adjusted Gross Income? " income
 
-if [ $income -lt 9226 ] ; then echo "10" ; nextBracket #15percent $income
-elif [ $income -lt 37451 ] ; then echo "15" ; nextBracket #25percent $income
-elif [ $income -lt 90751 ] ; then echo "25" ; nextBracket #28percent $income
-elif [ $income -lt 189301 ] ; then echo "28" ; nextBracket #33percent $income
-elif [ $income -lt 411501 ] ; then echo "33" ; nextBracket #35percent $income
-elif [ $income -lt 413201 ] ; then echo "35" ; nextBracket #39.6percent $income
-elif [ $income -gt 413200 ] ; then echo "39.6"
+if [ $income -lt 9226 ] ; then nextBracket $income 9226 10
+elif [ $income -lt 37451 ] ; then nextBracket $income 37451 15
+elif [ $income -lt 90751 ] ; then nextBracket $income 90751 25
+elif [ $income -lt 189301 ] ; then nextBracket $income 189301 28
+elif [ $income -lt 411501 ] ; then nextBracket $income 411501 33
+elif [ $income -lt 413201 ] ; then nextBracket $income 413201 35
+elif [ $income -gt 413200 ] ; then echo "39.6" ; echo "there is nothing more"
 else echo "invalid imput"
 fi

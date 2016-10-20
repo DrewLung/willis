bf0ca7171718fe63997a54f2764d31dd9f6ad423
Cloud User <ec2-user@ip-172-31-33-138.us-west-2.compute.internal>
2016-10-20 19:11:40 -0400

    I like soccer

--- weatherapi.sh
+++ weatherapi.sh
@@ -1,5 +1,7 @@
-echo $1
-name=$1
-echo $name
-#!/bin/bash
-curl -s "http://api.openweathermap.org/data/2.5/weather?q=Minneapolis,us&units=imperial&APPID=e7bfeb750ae444f9e21a02fdc668e776"
+i=$1
+
+while [ 1==1 ]
+sleep seq 60
+#$i+1
+#if $i=10
+done

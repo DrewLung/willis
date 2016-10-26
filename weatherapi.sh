005c2064e9b714620d69f0f0146075de8b0a430e
Cloud User <ec2-user@ip-172-31-33-138.us-west-2.compute.internal>
2016-10-26 08:08:47 -0400

    homework assignment

--- weatherapi.sh
+++ weatherapi.sh
@@ -1,8 +1,10 @@
+read date temp humidity <<< $(cat /tmp/weather.json | jq '.dt,.main.temp,.main.humidity')
+
 read weatherapi_key <<< $(cat weatherapi_key)
+
 echo $weatherapi_key
 set -x
-curl http://api.openweathermap.org/data/2.5/weatherq=Minneapolis7,us&units=imperial&APPID=$weatherapi_key
-
+curl "http://api.openweathermap.org/data/2.5/weather?q=Minneapolis7,us&units=imperial&APPID=$weatherapi_key"
 
 #while [1..10]
 #do echo "curl"

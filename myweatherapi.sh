cd13e32702cc8ac978b2cb2744d1e79916b2fd58
Cloud User <ec2-user@ip-172-31-33-138.us-west-2.compute.internal>
2016-11-01 15:44:42 -0400

    homework

---
+++ myweatherapi.sh
@@ -0,0 +1,41 @@
+#read temp humidity <<< $(cat weather.sample.json | jq '.main.temp,.main.humidity')
+
+#echo $temp $humidity
+
+#read weatherapi_key <<< $(cat weatherapi_key)
+
+#echo $weatherapi_key
+#set -x
+#curl "http://api.openweathermap.org/data/2.5/weather?q=Minneapolis7,us&units=imperial&APPID=$weatherapi_key"
+
+#while [1..10]
+#do echo "curl"
+#done
+
+#!/bin/bash
+read -p "How many days of samples: "               days
+read -p "N) New sample or A) Append to previous: " file_op
+
+if [ "$file_op" == "N" ]; then rm -f tmp; fi
+read samples <<< $(($days*24))
+read weatherapi_key <<< $(cat weatherapi_key)
+
+for i in $(seq 1 $samples); do
+
+  read epoch temp humidity <<< \
+    $(curl -s "http://api.openweathermap.org/data/2.5/weather?q=Minneapolis,us&units=imperial&APPID=$weatherapi_key" | \
+    jq '.dt,.main.temp,.main.humidity')
+
+  if [ $epoch -gt 1477570797 ]; then
+    read datetime <<<$(epoch -d @$epoch +'%m/%d/%Y %H:%M')
+    echo "$datetime temp=$temp humidity=$humidity"
+    echo $datetime,$temp,$humidity >> /tmp/weather_data.csv
+  else 
+    echo "$(date +'%m/%d/%Y %H:%M') bad sample"
+  fi
+
+  sleep 3600
+done
+
+#read date temp humidity <<< $(cat weather.json  | jq '.dt,.main.temp,.main.humidity')
+#read windspeed winddirection <<< $(cat /tmp/weather.json | jq '.wind.speed','.wind.deg')

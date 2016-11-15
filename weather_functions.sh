e3c46da1cdc7e86c069340d035039ee2066663c5
Cloud User <ec2-user@ip-172-31-33-138.us-west-2.compute.internal>
2016-11-15 14:39:57 -0500

    It needs another push

--- weather_functions.sh
+++ weather_functions.sh
@@ -1,9 +1,9 @@
 #the following will sum all the values of the temp_array and put them in a variable called 'sum'
 declare -a temp_array
 
-#function average {
-#  echo "$sum/" | bc -l
-#}
+function average {
+  echo "$sum/${temp_array[@]}" | bc 
+}
 
 for temp in $(cat ~/tmp/weather_data.csv | awk -F',' '{ print $2 }'); do 
   temp_array=("${temp_array[@]}" $temp)
